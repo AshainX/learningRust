995c03d6cc58d7f99a4ecb0ffcb8e8fc860728c0
Ashutosh Swain <79051730+AshainX@users.noreply.github.com>
2025-01-17 17:41:23 +0530

    shadowing with mutable vals

--- learning/src/shadowing.rs
+++ learning/src/shadowing.rs
@@ -2,6 +2,10 @@ pub fn shadowing_example() {
     let x = 5;
     println!("First value of x: {}", x);
 
+    let mut y = 4;
+    y+=2;
+    println!("After mutating with y as 4 then the mutated y is {}",y );
+
     let x = x + 2; 
     println!("After shadowing, value of x: {}", x);
 
